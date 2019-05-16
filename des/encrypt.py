797a0dc78b462c8a546d7f716811505d062e35eb
deceiver <deceiver@zalman.com>
2019-05-16 03:19:59 +0200

    (bi-bez)	[des]	lab03

---
+++ des/encrypt.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import pyDes, sys
+
+DATA_SIZE = 1024
+
+key    = ''
+mode   = ''
+iv     = ''
+chunks = 0
+enc    = []
+rest   = [] 
+
+# ------------------------------------------------
+# get key, mode, 
+while True:
+    inp = input ( "input 8byte long key: " )
+    if len ( inp ) != 8:
+        print ( "wrong length, try again!" )
+        continue
+    key = inp
+    break
+
+
+while True:
+    inp = input ( "which mode - ECB or CBC? " )
+    if inp != "ECB" and inp != "CBC":
+        print ( "wrong mode, try again! " )
+        continue
+    if inp == "ECB":
+        mode = pyDes . ECB
+    if inp == "CBC":
+        while True:
+            inv = input ( "you chose CBC, please provide 8byte long IV: " )
+            if len ( inv ) != 8:
+                print ( "wrong length, try again! " )
+                continue
+            iv = inv
+            break
+        mode = pyDes . CBC
+    break
+
+chunks = int ( input ( "how much 8byte chunks you want to encrypt?: " ) )
+if chunks == 0:
+    exit ( 1 )
+
+# ------------------------------------------------
+# setup the DES cipher
+des = pyDes . des ( key, mode, iv )
+# ------------------------------------------------
+pic = open ( "Mad_scientist.bmp", "rb" )
+
+stuff = pic . read ( 10 )
+
+#header = pic . read ( 2 )
+#size   = pic . read ( 4 )
+#reserv = pic . read ( 4 )
+offset = pic . read ( 4 )
+
+
+imgData = 0
+
+for byte in offset:
+    byte = int ( str ( byte ), 16 )
+    imgData += byte
+
+
+print ( "img data starts: ", str ( imgData - 14 ) )
+
+res = pic . read ( imgData )
+
+for chunk in range ( 0, chunks ):
+    data = pic . read ( 8 )
+    encrypted = des . encrypt ( data) 
+    enc . append ( encrypted )
+    print ( "original: ", data, end='')
+    print ( "  --> ", encrypted )
+
+while True:
+    data = pic . read ( DATA_SIZE )
+    if data == b'':
+        break
+    rest . append ( data )
+
+pic . close ()
+
+
+out = open ( "encrypted.bmp", "wb" )
+
+out . write ( stuff )
+out . write ( offset )
+out . write ( res )
+for chunk in enc:
+    out . write ( chunk )
+
+for chunk in rest:
+    out . write ( chunk )
+
+out . close ()
